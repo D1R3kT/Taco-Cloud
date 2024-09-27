dc5d4decc285259e44c7cb9128af478209186016
D1R3kT <egor0vdavid@yandex.ru>
2024-09-27 11:15:56 +0300

    Add Spring

---
+++ src/test/java/TacoCloudApplicationTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.TacoCloudApplication;
+
+@SpringBootTest(classes = TacoCloudApplication.class)
+public class TacoCloudApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
