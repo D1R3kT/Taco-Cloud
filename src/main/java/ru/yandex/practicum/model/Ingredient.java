9c73a9df96526ec631d828e616b606a79a466b6a
David Egorov <egor0vdavid@yandex.ru>
2025-01-28 16:00:49 +0300

    Merge branch 'main' into add-jdbc

--- src/main/java/ru/yandex/practicum/model/Ingredient.java
+++ src/main/java/ru/yandex/practicum/model/Ingredient.java
@@ -1,21 +1,21 @@
 package ru.yandex.practicum.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import lombok.*;
+
 import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
-@Table
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
+
 public class Ingredient {
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
+
 
     public enum Type {
         WRAP,
