d33179979cd7680cf3d3f589c9b1644e48303f03
David Egorov <egor0vdavid@yandex.ru>
2025-01-28 12:50:31 +0300

    add schema.sql

---
+++ src/main/java/ru/yandex/practicum/model/IngredientRef.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.model;
+
+import lombok.Data;
+
+@Data
+public class IngredientRef {
+    private final String ingredients;
+}
---
+++ src/main/java/ru/yandex/practicum/repository/jdbc/JdbcOrderRepository.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.model.Ingredient;
+import ru.yandex.practicum.model.Taco;
+import ru.yandex.practicum.model.TacoOrder;
+import ru.yandex.practicum.repository.OrderRepository;
+
+import java.beans.Transient;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class JdbcOrderRepository implements OrderRepository {
+
+    private final String SAVE_QUERY =
+            """
+            INSERT INTO taco_order (delivery_name, delivery_street, delivery_city, delivery_state, delivery_zip,
+            cc_number, cc_expiration, cc_cvv, placed_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """;
+
+    private JdbcOperations jdbcOperations;
+
+    public JdbcOrderRepository(JdbcOperations jdbcOperations) {
+        this.jdbcOperations = jdbcOperations;
+    }
+
+    @Override
+    @Transactional
+    public TacoOrder save(TacoOrder order) {
+        PreparedStatementCreatorFactory pscf =
+                new PreparedStatementCreatorFactory(SAVE_QUERY,
+                Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP
+        );
+        pscf.setReturnGeneratedKeys(true);
+
+        order.setPlacedAt(new Date());
+        PreparedStatementCreator psc =
+                pscf.newPreparedStatementCreator(
+                        Arrays.asList(
+                                order.getDeliveryName(),
+                                order.getDeliveryStreet(),
+                                order.getDeliveryCity(),
+                                order.getDeliveryState(),
+                                order.getDeliveryZip(),
+                                order.getCcNumber(),
+                                order.getCcExpiration(),
+                                order.getCcCVV(),
+                                order.getPlacedAt()));
+
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcOperations.update(psc, keyHolder);
+        long orderId = keyHolder.getKey().longValue();
+        order.setId(orderId);
+
+        List<Taco> tacos = order.getTacos();
+        int i = 0;
+        for (Taco taco : tacos) {
+            saveTaco(orderId, i++, taco);
+        }
+        return order;
+    }
+}
